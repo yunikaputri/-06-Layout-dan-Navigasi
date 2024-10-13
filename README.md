6ead6045b05a58b79a0e2dada932ff090cbd7268
Yunika Puteri Dwi Antika <yunikaputridwia@gmail.com>
2024-10-13 14:21:14 +0700

    Jobsheet 6_Praktikum 1: Membangun Layout di Flutter

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # layout_flutter
 
 A new Flutter project.
+
+# Praktikum 1: Layout dan Navigasi
+![Screenshot layout_flutter] (assets/praktikum1.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Widget get titleSection {
+    return Container(
+      // Soal 3: Menambahkan padding sebesar 32 di seluruh tepi
+      padding: const EdgeInsets.all(32.0),
+      child: Row(
+        children: [
+          // Soal 1: Expanded digunakan agar Column menyesuaikan ruang di dalam Row
+          Expanded(
+            child: Column(
+              // Mengatur crossAxisAlignment agar posisi berada di awal
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Soal 2: Baris pertama teks berada di dalam Container dengan padding
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                // Teks kedua dengan warna abu-abu
+                const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Soal 3: Icon bintang dengan warna merah
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          // Soal 3: Teks dengan angka 41
+          const Text('41'),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Flutter layout: Yunika Puteri Dwi Antika (2241760048)',
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        // Mengganti body 'Hello World' dengan titleSection
+        body: Column(
+          children: [
+            titleSection, // Menampilkan titleSection
+            const Center(
+              child: Text('Hello World'),
+            ),
+          ],
         ),
       ),
     );
